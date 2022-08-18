986c5678056f492cfcd744b044b64c59af66db41
safar ali <miqra019@gmail.com>
2022-08-18 18:58:14 +0500

    solve linters

--- my_list.rb
+++ my_list.rb
@@ -4,24 +4,24 @@ class MyList
   def initialize(*args)
     @list = args
   end
- 
+
   include MyEnumerable
- 
+
   def each
     @list.each { |el| yield(el) if block_given? }
   end
 end
- 
+
 # Create our list
 mylist = MyList.new(1, 2, 3, 4)
- 
+
 # Test #all?
 mylist.all? { |e| e < 5 }
 mylist.all? { |e| e > 5 }
- 
+
 # Test #any?
 mylist.any? { |e| e == 2 }
 mylist.any? { |e| e == 5 }
- 
+
 # Test #filter?
 mylist.filter(&:even?)
